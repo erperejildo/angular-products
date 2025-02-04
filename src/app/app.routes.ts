bbdf2ed30a7b29ed06afe2ab71476d2c1ad4be19
Daniel <d15_1_89@msn.com>
2025-02-04 16:43:41 +0100

    pages set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/product-list/product-list.component').then(
+        (m) => m.ProductListComponent
+      ),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./pages/favorites/favorites.component').then(
+        (m) => m.FavoritesComponent
+      ),
+  },
+];
